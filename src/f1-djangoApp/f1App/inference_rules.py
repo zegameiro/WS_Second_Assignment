14c98e7b34a9af5b7497540e42146728317790f7
zegameiro <jose.mcgameiro@ua.pt>
2025-06-09 19:57:14 +0100

    Added SPIN rules

--- src/f1-djangoApp/f1App/inference_rules.py
+++ src/f1-djangoApp/f1App/inference_rules.py
@@ -44,6 +44,9 @@ def apply_inference_rules():
     """
     db.update(query)
 
+def apply_plus_inference_rules():
+
+    # Add the total number of races that occured in each circuit
     query = f"""
         PREFIX pred: <{PRED}>
         PREFIX ps: <{NS}>
@@ -60,3 +63,85 @@ def apply_inference_rules():
         }}
     """
     db.update(query)
+
+    # Add a rule to infer the winner of a race
+    query = f"""
+        PREFIX pred: <{PRED}>
+        PREFIX ps: <{NS}>
+        INSERT {{
+            ?driver pred:hasWonRace ?race
+        }} 
+        WHERE {{
+            ?result a ps:Result ;
+                pred:hasDriver ?driver ;
+                pred:position "1"^^xsd:integer ;
+                pred:participatedIn ?race .
+        }}
+    """
+    db.update(query)
+
+    # Infer that a race belongs to a season
+    query = f"""
+        PREFIX pred: <{PRED}>
+        PREFIX ps: <{NS}>
+        INSERT {{
+            ?race pred:partOfSeason ?season .
+        }}
+        WHERE {{
+            ?race a ps:Race ;
+                pred:year ?year .
+            ?season a ps:Season ;
+                pred:url ?url . 
+            FILTER(CONTAINS(STR(?url), STR(?year))) 
+        }}
+    """
+    db.update(query)
+
+    # Infer driver's full name
+    query = f"""
+        PREFIX pred: <{PRED}>
+        PREFIX ps: <{NS}>
+        INSERT {{
+            ?driver pred:fullName ?fullName .
+        }}
+        WHERE {{
+            ?driver a ps:Driver ;
+                pred:forename ?forename ;
+                pred:surname ?surname .
+            BIND(CONCAT(?forename, " ", ?surname) AS ?fullName)
+        }}
+    """
+    db.update(query)
+
+    # Infer fastestDriver in a race based on the fastest Speed lap
+    query = f"""
+        PREFIX pred: <{PRED}>
+        PREFIX ps: <{NS}>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+        INSERT {{
+            ?driver pred:fastestInRace ?race .
+        }}
+        WHERE {{
+            {{
+                SELECT ?race (MAX(xsd:decimal(?speedVal)) AS ?maxSpeed)
+                WHERE {{
+                    ?result a ps:Result ;
+                        pred:participatedIn ?race ;
+                        pred:fastestLapSpeed ?speedRaw .
+                    BIND(xsd:decimal(?speedRaw) AS ?speedVal)
+                }}
+                GROUP BY ?race
+            }}
+
+            ?result a ps:Result ;
+                pred:participatedIn ?race ;
+                pred:hasDriver ?driver ;
+                pred:fastestLapSpeed ?driverSpeedRaw .
+            BIND(xsd:decimal(?driverSpeedRaw) AS ?driverSpeed)
+            FILTER(?driverSpeed = ?maxSpeed)
+        }}
+    """
+    db.update(query)
+
+
